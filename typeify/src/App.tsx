9fbaf4355e7432171649351080c1eb8258836d29
aj <arjun.menon2299@gmail.com>
2022-03-20 09:08:58 +1000

    commit 3, added state saving functionality

--- typeify/src/App.tsx
+++ typeify/src/App.tsx
@@ -2,23 +2,30 @@ import React, { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import "./components/styles.css";
+import TodoList from "./components/TodoList";
 import { Todo } from "./models/model";
-
-interface Props {
-  todo: string;
-  setTodo: Dispatch<SetStateAction<string>>;
-}
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState<Array<Todo>>([]);
 
-  console.log(todo);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- typeify/src/components/InputField.tsx
+++ typeify/src/components/InputField.tsx
@@ -1,18 +1,28 @@
-import React, { SetStateAction } from "react";
+import React, { SetStateAction, useRef } from "react";
 import "./styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
-        type="input"
+        type="text"
+        placeholder="Enter a Task"
         value={todo}
+        ref={inputRef}
         onChange={(e) => setTodo(e.target.value)}
-        placeholder="enter a task"
         className="input__box"
       />
       <button className="input_submit" type="submit">
